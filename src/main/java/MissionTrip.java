defd78202425406c9dfbb473192222c226a8017d
chen <cc5182smile@gmail.com>
2019-05-24 13:56:02 -0700

    improved

--- src/main/java/MissionTrip.java
+++ src/main/java/MissionTrip.java
@@ -1,27 +1,31 @@
 public class MissionTrip {
-    int conditions[][] = new int[5][5];
-    int ways[][] = new int[5][5];
+    int conditions[][];
+    int ways[][];
+    int paths[][];
 
 
     public int resolve(int[][] paths) {
-        calculate(0, 0, paths);
+        this.paths = paths;
+        this.conditions = new int[paths.length][paths[0].length];
+        this.ways = new int[paths.length][paths[0].length];
+        calculate(0, 0);
         return ways[paths.length - 1][paths[0].length - 1];
     }
 
-    private void calculate(int i, int j, int[][] paths) {
+    private void calculate(int i, int j) {
         conditions[i][j] = 1;
         ways[i][j]++;
         if (i >= 1 && conditions[i - 1][j] != 1 && paths[i - 1][j] == 0) {
-            calculate(i - 1, j, paths);
+            calculate(i - 1, j);
         }
         if (i < paths.length - 1 && conditions[i + 1][j] != 1 && paths[i + 1][j] == 0) {
-            calculate(i + 1, j, paths);
+            calculate(i + 1, j);
         }
         if (j >= 1 && conditions[i][j - 1] != 1 && paths[i][j - 1] == 0) {
-            calculate(i, j - 1, paths);
+            calculate(i, j - 1);
         }
         if (j < paths[0].length - 1 && conditions[i][j + 1] != 1 && paths[i][j + 1] == 0) {
-            calculate(i, j + 1, paths);
+            calculate(i, j + 1);
         }
         conditions[i][j] = 0;
 
@@ -29,13 +33,11 @@ public class MissionTrip {
 
     public static void main(String[] args) {
         //long a=System.currentTimeMillis();
-
         MissionTrip missionTrip = new MissionTrip();
-        int[][] map = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}};
+        //int[][] map = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}};
         //int[][] map = {{0, 0, 0}, {0, 0, 1}, {0, 1, 0}};
-        //int[][] map = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+        int[][] map = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
         System.out.println(missionTrip.resolve(map));
-
         //System.out.println((System.currentTimeMillis()-a)/1000f+ "秒");
     }
 
